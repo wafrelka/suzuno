39f8dc063df5a4e10dc75aebd2adbf7664fc8d68
wafrelka <wafrelka@gmail.com>
2019-12-22 01:59:14 +0900

    Stop hiding the toolboxes during the switching to the list
    This makes the rendering more smooth,
    since the toolbox deactivation causes repainting of the pager.

--- resources/pager.js
+++ resources/pager.js
@@ -328,7 +328,6 @@ class Pager {
 		for(let img_elem of this._root.querySelectorAll(".pager-page-image")) {
 			reset_img_src_if_incomplete(img_elem);
 		}
-		this.hide_toolbox();
 	}
 
 	update(resources) {
